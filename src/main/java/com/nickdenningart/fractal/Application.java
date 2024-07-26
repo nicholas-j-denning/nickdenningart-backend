7dd65886afa31f58cf7746b742fe871f08c8ccc6
Nick Denning <nicholas.j.denning@gmail.com>
2024-07-26 16:46:14 -0700

    Removed component scanning

--- src/main/java/com/nickdenningart/fractal/Application.java
+++ src/main/java/com/nickdenningart/fractal/Application.java
@@ -1,9 +1,29 @@
 package com.nickdenningart.fractal;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+import com.nickdenningart.fractal.controller.FractalController;
+import com.nickdenningart.fractal.controlleradvice.ControllerExceptionHandler;
+import com.nickdenningart.fractal.service.AuthorizationService;
+import com.nickdenningart.fractal.service.FractalService;
+import com.nickdenningart.fractal.service.GalleryService;
+import com.nickdenningart.fractal.service.ImageService;
+import com.nickdenningart.fractal.service.PrintifyService;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@Import({
+	FractalController.class, 
+	ControllerExceptionHandler.class,
+	AuthorizationService.class,
+	FractalService.class,
+	GalleryService.class,
+	ImageService.class,
+	PrintifyService.class
+})
 public class Application {
 
 	public static void main(String[] args) {
